ee656193c7185cbbf6aee8d1c1bb8681c0551459
Panagiotis Tzortzis <tzortzispanagiotis@gmail.com>
2021-05-27 14:18:51 +0300

    initial commit

---
+++ src/main/java/com/ails/stirdatabackend/StirdataBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ails.stirdatabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StirdataBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StirdataBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ails/stirdatabackend/StirdataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ails.stirdatabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StirdataBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
