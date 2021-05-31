6a3385460f9a0fadc9976b309252c112a5b21e48
Panagiotis Tzortzis <tzortzispanagiotis@gmail.com>
2021-05-31 17:36:17 +0300

    endpoint manager sorts list of uris with corresponding sparql endpoint

--- src/main/java/com/ails/stirdatabackend/model/EndpointManager.java
+++ src/main/java/com/ails/stirdatabackend/model/EndpointManager.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Getter
@@ -26,5 +28,19 @@ public class EndpointManager {
             }
         }
         return res;
+    }
+
+    public HashMap<SparqlEndpoint, List<String>> getEndpointsByNuts(List<String> nutsUri) {
+        HashMap<SparqlEndpoint, List<String>> response = new HashMap<SparqlEndpoint, List<String>>();
+        for (String uri : nutsUri) {
+            SparqlEndpoint tmp = getEndpointFromNutsUri(uri);
+            if (response.containsKey(tmp)) {
+                response.get(tmp).add(uri);
+            }
+            else {
+                response.put(tmp, new ArrayList<String>());
+            }
+        }
+        return response;
     }
 }
