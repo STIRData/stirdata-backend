6c57978c387113cbd773f38b11b76a2da479c2f8
Panagiotis Tzortzis <tzortzispanagiotis@gmail.com>
2021-09-28 13:50:17 +0300

    added spring security stuff

---
+++ src/main/java/com/ails/stirdatabackend/security/SecurityConfiguration.java
@@ -0,0 +1,7 @@
+package com.ails.stirdatabackend.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+}
