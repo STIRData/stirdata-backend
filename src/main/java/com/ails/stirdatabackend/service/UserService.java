eaf56cc9e854118b775f50fadfea67bf09b8bd35
Jason <g.liartis@gmail.com>
2021-09-29 13:07:09 +0300

    comment out stuff

--- src/main/java/com/ails/stirdatabackend/service/UserService.java
+++ src/main/java/com/ails/stirdatabackend/service/UserService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import static java.lang.String.format;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserService {
 
     @Autowired
     private UserRepository userRepository;
