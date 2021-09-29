7f3555e5b3bc4eaf0527c7e5c7bde6d23d7e68eb
Panagiotis Tzortzis <tzortzispanagiotis@gmail.com>
2021-09-29 12:34:30 +0300

    mongo up and running

--- src/main/java/com/ails/stirdatabackend/model/User.java
+++ src/main/java/com/ails/stirdatabackend/model/User.java
@@ -1,7 +1,26 @@
 package com.ails.stirdatabackend.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
 public class User {
+
+        @Id
+        private ObjectId id;
+        private String email;
+        private String password;
+        private String firstName;
+        private String lastName;
 
 }
--- src/main/java/com/ails/stirdatabackend/repository/UserRepository.java
+++ src/main/java/com/ails/stirdatabackend/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.ails.stirdatabackend.repository;
 
+import com.ails.stirdatabackend.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository {
+//@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
 }
