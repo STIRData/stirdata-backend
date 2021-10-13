5d6c01df5f59bb99b47c75eaa9b8c885949cf6a1
Panagiotis Tzortzis <tzortzispanagiotis@gmail.com>
2021-10-13 15:33:20 +0300

    edited path for oauth, now /api/oauth

--- src/main/java/com/ails/stirdatabackend/security/OAuthController.java
+++ src/main/java/com/ails/stirdatabackend/security/OAuthController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/oauth")
+@RequestMapping("/api/oauth")
 public class OAuthController {
 
     @Autowired
