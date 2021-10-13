da14cc38b6df245f10c8a6a66a7c2db78b80906a
Panagiotis Tzortzis <tzortzispanagiotis@gmail.com>
2021-10-13 16:12:43 +0300

    minor change, added import

--- src/main/java/com/ails/stirdatabackend/controller/NutsController.java
+++ src/main/java/com/ails/stirdatabackend/controller/NutsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
 @RestController
