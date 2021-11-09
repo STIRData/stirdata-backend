432f9804490f57854104337285e8a64453a8c392
achort <achort@hotmail.com>
2021-11-09 14:34:18 +0200

    missing class

---
+++ src/main/java/com/ails/stirdatabackend/configuration/ModelConfiguration.java
@@ -0,0 +1,29 @@
+package com.ails.stirdatabackend.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Getter
+@Setter
+public class ModelConfiguration {
+    private final static Logger logger = LoggerFactory.getLogger(ModelConfiguration.class);
+
+    private String model;
+    
+    private String url;
+    
+    private String entitySparql;
+    private String entityNameSparql;
+    private String nuts3Sparql;
+    private String naceSparql;
+    private String lauSparql;
+    private String foundingDateSparql;
+    
+    public ModelConfiguration(String model) {
+    	this.model = model;
+    }
+    
+    
+}
