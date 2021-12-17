4de9deacd10038f90422d42c631916fd6dfa413b
Panagiotis Tzortzis <tzortzispanagiotis@gmail.com>
2021-12-17 15:50:18 +0200

    fixed log4j lol

--- src/main/java/com/ails/stirdatabackend/model/User.java
+++ src/main/java/com/ails/stirdatabackend/model/User.java
@@ -2,10 +2,7 @@ package com.ails.stirdatabackend.model;
 
 
 import com.ails.stirdatabackend.payload.GoogleAccountUserInfoDTO;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
