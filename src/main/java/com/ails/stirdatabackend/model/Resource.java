565fd823d47000be8e91c2b0c8e12ed0196a0f48
achort <achort@hotmail.com>
2022-02-23 17:31:43 +0200

    resource class

---
+++ src/main/java/com/ails/stirdatabackend/model/Resource.java
@@ -0,0 +1,17 @@
+package com.ails.stirdatabackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Resource {
+
+	private String uri;
+	private String label;
+	
+}
