10a84db08767ec1ccf43347e92d68c7fba20c341
tzortzis <tzortzispanagiotis@gmail.com>
2022-04-11 11:45:32 +0300

    fixed security config with authenticationManager

--- src/main/java/com/ails/stirdatabackend/security/SecurityConfiguration.java
+++ src/main/java/com/ails/stirdatabackend/security/SecurityConfiguration.java
@@ -1,25 +1,33 @@
 package com.ails.stirdatabackend.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @EnableWebSecurity()
 @EnableGlobalMethodSecurity(
         securedEnabled = true
 )
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImplementation userDetailsServiceImplementation;
 
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
@@ -30,12 +38,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Value("${app.http.cors.max-age}")
     private long corsMaxAgeSeconds;
 
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
+    @Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsServiceImplementation)
+                .passwordEncoder(passwordEncoder());
+    }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         // Enable CORS and disable CSRF
-        // TODO: why?
         http = http.cors().and().csrf().disable();
 
         http = http
