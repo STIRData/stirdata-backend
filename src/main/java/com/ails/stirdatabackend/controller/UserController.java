6a780c31a03a30170ee9ced0a6fa5c3420f8d5e7
tzortzis <tzortzispanagiotis@gmail.com>
2022-11-18 15:16:29 +0200

    [SD-15] add call for update, delete user account

--- src/main/java/com/ails/stirdatabackend/controller/UserController.java
+++ src/main/java/com/ails/stirdatabackend/controller/UserController.java
@@ -1,10 +1,11 @@
 package com.ails.stirdatabackend.controller;
 
 import java.util.Optional;
-
+import java.util.Map;
 import javax.validation.Valid;
 
 import com.ails.stirdatabackend.model.User;
+import com.ails.stirdatabackend.model.UserLoginType;
 import com.ails.stirdatabackend.payload.AuthenticationResponse;
 import com.ails.stirdatabackend.payload.LoginRequestDTO;
 import com.ails.stirdatabackend.payload.Message;
@@ -19,6 +20,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -77,6 +81,55 @@ public class UserController {
         catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }  
+    }
+
+    @PutMapping("/updateUserDetails")
+    public ResponseEntity<?> updateUserDetails(@AuthenticationPrincipal UserPrincipal currentUser, @RequestBody Map<String, String> userDetailsUpdate) {
+        try {
+            Optional<User> userOpt = userService.getUserById(currentUser.getId().toString());
+            if (!userOpt.isPresent()) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+            }
+            User user = userOpt.get();
+            if (userDetailsUpdate.get("firstName") != null) {
+                user.setFirstName(userDetailsUpdate.get("firstName"));
+            }
+            if (userDetailsUpdate.get("lastName") != null) {
+                user.setLastName(userDetailsUpdate.get("lastName"));
+            }
+            if (userDetailsUpdate.get("organization") != null) {
+                user.setOrganization(userDetailsUpdate.get("organization"));
+            }
+            if (userDetailsUpdate.get("email") != null) {
+                user.setEmail(userDetailsUpdate.get("email"));
+            }
+            if (userDetailsUpdate.get("password") != null && user.getUserLoginType() == UserLoginType.CUSTOM) {
+                user.setPassword(userService.encodePassword(userDetailsUpdate.get("password")));
+            }
+
+            userService.saveUser(user);
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }  
+    }
+
+    @DeleteMapping("/deleteAccount")
+    public ResponseEntity<?> deleteUserAccount(@AuthenticationPrincipal UserPrincipal currentUser) {
+        try{
+            Optional<User> userOpt = userService.getUserById(currentUser.getId().toString());
+            if (!userOpt.isPresent()) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+            }
+            User user = userOpt.get();
+            userService.deleteUser(user);
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        } 
     }
 
 
